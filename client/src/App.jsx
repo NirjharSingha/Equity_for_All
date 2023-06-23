ac71795e37cf9d8d691761604b1977e9427dd0b0
Nirjhar <nirjharsingha@gmail.com>
2023-06-23 06:01:30 +0600

    footer component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import Home from "./components/Home";
+// import Home from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Footer />
     </div>
   );
