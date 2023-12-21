dd889a0fdf1e3b69222f490467d5f27ebaad9328
Nirjhar <nirjharsingha@gmail.com>
2023-12-21 15:44:50 +0600

    cors in sse fixing

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,10 +2,11 @@ import express from "express";
 import commentSSE from "../controllers/api/commentSSE.js";
 import deleteFile from "../controllers/api/deleteFile.js";
 import searchResult from "../controllers/api/searchResult.js";
+import cors from "cors";
 
 const router = express.Router();
 
-router.get("/commentSSE", commentSSE);
+router.get("/commentSSE", cors(), commentSSE);
 router.get("/searchResult", searchResult);
 router.delete("/deleteFile", deleteFile);
 
