44ee6e3402e3c363f3e49dc32d21458f456f7d74
Nirjhar <nirjharsingha@gmail.com>
2023-12-21 19:12:06 +0600

    cors in sse fixing

--- server/controllers/api/commentSSE.js
+++ server/controllers/api/commentSSE.js
@@ -3,27 +3,27 @@ import { addClient, removeClient } from "../../utils/sse.js";
 
 const commentSSE = asyncHandler(async (req, res) => {
   // Add CORS headers
-  // res.setHeader("Access-Control-Allow-Credentials", true);
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, OPTIONS, POST, PUT, DELETE"
-  // );
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  // );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, POST, PUT, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 
-  // if (req.method === "OPTIONS") {
-  //   res.status(200).end();
-  //   return;
-  // }
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
 
-  // res.setHeader("Content-Type", "text/event-stream");
-  // res.setHeader("Cache-Control", "no-cache");
-  // res.setHeader("Connection", "keep-alive");
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
 
-  // res.status(200).flushHeaders();
+  res.status(200).flushHeaders();
 
   addClient(res);
 
--- server/index.js
+++ server/index.js
@@ -45,12 +45,12 @@ app.use(cors());
 
 //   next();
 // });
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
