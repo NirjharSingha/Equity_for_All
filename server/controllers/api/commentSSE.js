440cf11416ce92fa4a7fbeebed19ec2c53a4994b
Nirjhar <nirjharsingha@gmail.com>
2023-12-21 15:28:10 +0600

    cors in sse fixing

--- server/controllers/api/commentSSE.js
+++ server/controllers/api/commentSSE.js
@@ -3,9 +3,21 @@ import { addClient, removeClient } from "../../utils/sse.js";
 
 const commentSSE = asyncHandler(async (req, res) => {
   // Add CORS headers
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, POST, PUT, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
 
   res.writeHead(200, {
     "Content-Type": "text/event-stream",
